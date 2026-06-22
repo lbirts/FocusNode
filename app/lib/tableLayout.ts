f23722370cab063c5c116511457c2f8158eda93a
lbirts <birtslauren@gmail.com>
2026-06-22 16:00:01 -0700

    workload-table-autolayout

--- app/lib/tableLayout.ts
+++ app/lib/tableLayout.ts
@@ -1,3 +1,3 @@
 import type { CSSProperties } from "react";
 
-export const WORKLOAD_TABLE_LAYOUT: CSSProperties["tableLayout"] = "fixed";
+export const WORKLOAD_TABLE_LAYOUT: CSSProperties["tableLayout"] = "auto";
